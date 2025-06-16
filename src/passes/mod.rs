e3e3fa55c87c30195112a64ba2fcef5130ffbca0
Felix Suchert <dev@felixsuchert.de>
2025-06-16 17:13:21 +0200

    Fix constant folding pass

--- src/passes/mod.rs
+++ src/passes/mod.rs
@@ -3,48 +3,59 @@ use std::ops::Neg;
 use crate::types::{Expr, Operator};
 
 pub trait ConstantFold {
-    fn run_constant_fold(&mut self);
+    fn run_constant_fold(self) -> Self;
 }
 
 impl ConstantFold for Expr {
-    fn run_constant_fold(&mut self) {
+    fn run_constant_fold(self) -> Self {
         println!("{self:?}");
         match self {
             Expr::Num(_) | Expr::Var(_) =>
             /* no work to be done */
             {
-                ()
+                self
             }
             Expr::UnaryOp(operator, expr) => {
-                if !expr.is_num() {
-                    expr.run_constant_fold();
-                }
+                let e = if !expr.is_num() {
+                    expr.run_constant_fold()
+                } else {
+                    *expr
+                };
 
-                if let Expr::Num(n) = expr.as_mut() {
-                    if operator == &Operator::Sub {
-                        *self = Expr::Num(n.neg());
+                if let Expr::Num(n) = e {
+                    if operator == Operator::Sub {
+                        return Expr::Num(n.neg());
                     }
                 }
+
+                Expr::UnaryOp(operator, Box::new(e))
             }
             Expr::BinaryOp(lhs, operator, rhs) => {
-                if !lhs.is_num() {
-                    lhs.run_constant_fold();
-                }
-                if !rhs.is_num() {
-                    rhs.run_constant_fold();
-                }
+                let l = if !lhs.is_num() {
+                    lhs.run_constant_fold()
+                } else {
+                    *lhs
+                };
 
-                if let Expr::Num(left) = lhs.as_mut() {
-                    if let Expr::Num(right) = rhs.as_mut() {
+                let r = if !rhs.is_num() {
+                    rhs.run_constant_fold()
+                } else {
+                    *rhs
+                };
+
+                if let Expr::Num(left) = l {
+                    if let Expr::Num(right) = r {
                         let res = match operator {
-                            Operator::Add => *left + *right,
-                            Operator::Sub => *left - *right,
-                            Operator::Mul => *left * *right,
-                            Operator::Div => *left / *right,
+                            Operator::Add => left + right,
+                            Operator::Sub => left - right,
+                            Operator::Mul => left * right,
+                            Operator::Div => left / right,
                         };
-                        *self = res.into();
+                        return res.into();
                     }
                 }
+
+                Expr::BinaryOp(Box::new(l), operator, Box::new(r))
             }
         }
     }
