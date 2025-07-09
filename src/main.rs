846879fdac5c2d171c51305b45680b2ff0257eac
Mees Frensel <meesfrensel@gmail.com>
2025-07-09 12:48:41 +0200

    Add app icon
    This doesn't work on gnome so it is actually untested :p
    See https://github.com/emilk/egui/issues/3992

--- src/main.rs
+++ src/main.rs
@@ -6,16 +6,28 @@ mod parser;
 mod passes;
 mod types;
 
+use std::sync::Arc;
 use crate::app::LndwApp;
 use rust_i18n::t;
 
 rust_i18n::i18n!("locales", fallback = "en");
 
+const APP_NAME: &str = "tud-ccc-demo-compiler";
+
 fn main() {
+    let icon = eframe::icon_data::from_png_bytes(include_bytes!("../assets/icon.png"));
+    let icon = icon.ok().map(|i| Arc::new(i));
+    if icon.is_none() {
+        eprintln!("Warning: failed to load icon");
+    }
+
     let mut native_options = eframe::NativeOptions::default();
     native_options.viewport.maximized = Some(true);
+    native_options.viewport.title = Some(t!("app.default_name").into());
+    native_options.viewport.icon = icon;
+
     let _ = eframe::run_native(
-        &t!("app.name"),
+        APP_NAME,
         native_options,
         Box::new(|cc| Ok(Box::new(LndwApp::new(cc)))),
     );
